7b7b605638f3060d354d9024ee24467f3f170373
thecareer <jimmy@career.vn>
2017-10-01 18:58:42 +0700

    ok

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
                 <h2 class="box-title">Startups in Vietnam</h2>
 
                 <div class="clearfix text-formatted field field--name-body field--type-text-with-summary field--label-hidden field__item">
-                    <p>StartupsIn Vietnam is the online community
+                    <p>Startup.JOBS is the job post site
                         <br> for startups and tech companies.
                         <br> Find startup jobs, company and startup blog.</p>
                     <p>© Startup.JOBS 2017</p>
@@ -32,3 +32,85 @@
                     </li>
                     <li class="menu-item">
                         <a href="#" target="_blank">Contact Us</a>
+
+                    </li>
+                </ul>
+
+            </nav>
+
+        </div>
+
+        <div class="region region-footer-third">
+            <nav role="navigation" aria-labelledby="block-about-menu" id="block-about" class="block block-menu navigation menu--about">
+
+                <h2 class="box-title" id="block-about-menu">About</h2>
+
+                <ul class="menu">
+                    <li class="menu-item">
+                        <a href="#" target="_blank">Careers</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#" target="_blank">Privacy Policy</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#" target="_blank">Terms of Use</a>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </div>
+
+        <div class="region region-footer-fourth">
+            <nav role="navigation" aria-labelledby="block-stayconnected-menu" id="block-stayconnected" class="block block-menu navigation menu--stay-connected">
+
+                <h2 class="box-title" id="block-stayconnected-menu">Stay Connected</h2>
+
+                <ul class="menu">
+                    <li class="menu-item">
+                        <a href="https://www.facebook.com/go.startup.jobs" target="_blank">Facebook</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="http://twitter.com/gostartupjobs" target="_blank">Twitter</a>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </div>
+
+        <div class="region region-footer-fifth">
+            <nav role="navigation" aria-labelledby="block-poweredbybuiltin-menu" id="block-poweredbybuiltin" class="block block-menu navigation menu--powered-by-built-in">
+
+                <h2 class="box-title" id="block-poweredbybuiltin-menu">Our communities</h2>
+
+                <ul class="menu">
+                    <li class="menu-item">
+                        <a href="//startup.jobs/sg" target="_blank">Startup.JOBS In Singapore</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="//startup.jobs/vn" target="_blank">Startup.JOBS In Vietnam</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="//startup.jobs/id" target="_blank">Startup.JOBS In Indonesia</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="//startup.jobs/my" target="_blank">Startup.JOBS In Malaysia</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="//startup.jobs/th" target="_blank">Startup.JOBS In Thailand</a>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </div>
+
+    </div>
+</footer>
+<div class="overlay"></div>
+<div class="basement">Love your Startup <span></span> Love your job</div>
+</div>
+    <?php wp_footer() ?>
+    </body>
+</html>
