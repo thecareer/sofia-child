1446479fa11e5d011b7d1f6c73cc691f250c4709
dakachi <david87dang@gmail.com>
2017-10-07 19:16:21 +0700

    update modal apply job

--- js/map-detail.js
+++ js/map-detail.js
@@ -27,15 +27,18 @@
         // When the user clicks the button, open the modal 
         btn.click(function() {
             modal.style.display = "block";
+            $('body').addClass('modal-open');
         });
         // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
             modal.style.display = "none";
+            $('body').removeClass('modal-open');
         }
         // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
+                $('body').removeClass('modal-open');
             }
         }
         initMapDetail();
