cd6f3bd1cd4157a712e9deea056ab6688fb47057
dakachi <david87dang@gmail.com>
2017-10-29 19:06:05 +0700

    update modal apply job

--- fragment/modal-apply.php
+++ fragment/modal-apply.php
@@ -1,9 +1,18 @@
 <?php if(is_singular( 'job' )) :?>
+<script>
+  function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+  }
+</script>
 <div id="apply-modal" class="modal">
 
   <!-- Modal content -->
   <div class="modal-content">
-    <iframe src="https://apply.startup.jobs/apply/embed/form/<?php echo vp_metabox('jobplanet_job.jazzid'); ?>"></iframe>
+  	<div class="modal-header">
+        <span class="close">&times;</span>
+    </div>
+    <iframe onload="resizeIframe(this)" src="https://apply.startup.jobs/apply/embed/form/<?php echo vp_metabox('jobplanet_job.jazzid'); ?>" frameborder="0" scrolling="no"></iframe>
   </div>
 </div>
+
 <?php endif; ?>
