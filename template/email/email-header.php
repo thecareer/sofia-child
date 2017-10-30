be7cd5add57e0eb77f2cddad94d5b36fddaef27f
dakachi <david87dang@gmail.com>
2017-10-30 23:43:14 +0700

    add email image

--- template/email/email-header.php
+++ template/email/email-header.php
@@ -1,13 +1,19 @@
-<table border="0" cellpadding="0" cellspacing="0" width="100%">
-    <tbody>
-    <tr>
-        <td>
-            <a href="<?php esc_html(home_url()) ?>" style="display:block;margin-bottom:10px" target="_blank">
-                <img src="<?php echo $header_logo; ?>" alt="<?php echo esc_html($blogname); ?>" title="<?php echo esc_html($blogname); ?>" border="0">
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td style="border:1px solid #ccc;">
-            <table style="line-height:normal;font:13px arial" border="0" cellpadding="0" cellspacing="0" width="100%">
-                <tbody>
+<div style="background:#fff;padding:0;font-family:'Open Sans',Open Sans,sans-serif!important">
+    <table align="center" bgcolor="#2b333a" border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tbody>
+            <tr>
+                <td>
+                    <table style="margin:20px auto 0;background:transparent;border-collapse:collapse;text-align:center" width="600">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div style="font-size:16px;line-height:16px">
+                                    </div>
+                                    <img height="38" src="<?php echo get_stylesheet_directory_uri(); ?>/img/email-logo.png">
+                                        <div style="font-size:36px;line-height:36px">
+                                        </div>
+                                    </img>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
