ec1ba37c14d6c8071f264b85fe687be809e75146
Pham Quoc Thang <quocthang@career.vn>
2017-11-07 13:50:30 +0700

    page embed

--- page-embed.php
+++ page-embed.php
@@ -2,5 +2,27 @@
 /**
  * Template Name: Embed Page
  */
-var_dump($_REQUEST);
+$uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri_segments = explode('/', $uri_path);
+
+if(isset($uri_segments[2]) && $uri_segments[2] != null) {
+	$code = $uri_segments[2];
+
+	$args = array(
+		'posts_per_page'   => 1,
+		'offset'           => 0,
+		'meta_key'         => 'embedcode',
+		'meta_value'       => $code,
+		'post_type'        => 'job',
+		'post_status'      => 'publish',
+	);
+	$posts_array = get_posts( $args );
+	if(count($posts_array) > 0) {
+		
+		wp_redirect(get_post_permalink($posts_array[0]->ID));
+		exit();	
+	}
+}
+wp_redirect(home_url());
+exit();
 ?>
