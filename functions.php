0f211e8a7eedddac0b9776dc9f505bfcb78a43f4
dakachi <david87dang@gmail.com>
2017-11-12 22:25:22 +0700

    update single company

--- functions.php
+++ functions.php
@@ -510,8 +510,12 @@ function staticize_attachment_src($image, $attachment_id, $size, $icon)
             $link .= '/w_424,h_317,c_fill';
             break;
 
-        case 'large' : 
+        case 'medium' : 
             $link .= '/w_424,h_317,c_fill';
+            break;
+
+        case 'large' : 
+            $link .= '/w_809,h_450,c_fill';
             break;
         case 'full':
             return $image;
